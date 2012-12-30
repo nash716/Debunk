280f217816835a4b8b47b990d08a1a740f6fa455
nash <contact@nash-dev.com>
2012-12-30 12:15:12 +0900

    設定画面仮完成

--- src/settings/js/main.js
+++ src/settings/js/main.js
@@ -1,5 +1,37 @@
 onload = main;
 
-function main() {
+var confKey = {
+	port: '#input-port'
+};
 
+function main() {
+	window.utils = require('../js/utils');
+
+	$('#save').click(save);
+	$('#cancel').click(_close);
+
+	var conf = utils.getAll();
+
+	for (var key in conf) {
+		if (conf[key].user === false) continue;
+
+		if (conf[key].type == 'object') {
+			// [TODO]
+		} else {
+			$(confKey[key]).val(conf[key].value);
+		}
+	}
+}
+
+function save() {
+	for (var key in confKey) {
+		utils.config(key, $(confKey[key]).val());
+	}
+
+	_close();
+	return false;
+}
+
+function _close() {
+	require('nw.gui').Window.get().close();
 }
