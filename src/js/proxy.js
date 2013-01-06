56bbd55b56116368053858f4b5f8860029b4a55f
nash <contact@nash-dev.com>
2013-01-06 10:05:26 +0900

    onRequest 部分の API 実装完了

--- src/js/proxy.js
+++ src/js/proxy.js
@@ -1,11 +1,17 @@
 var http = require('http'),
 	utils = require('./utils'),
 	view = require('./view'),
-	ProxyRequest = require('./ProxyRequest');
+	ProxyRequest = require('./ProxyRequest'),
+	ExtensionManager = require('./ExtensionManager'),
+	ExtensionType = require('./ExtensionType');
 
 module.exports = exports = function() {
 	http.createServer(function(req, res) {
 		var parsedURL = require('url').parse(req.url);
+
+		var result = executeExtension(req);
+
+		req.headers = result.headers;
 
 		var proxyRequest = new ProxyRequest(req, res);
 
@@ -13,9 +19,25 @@ module.exports = exports = function() {
 			proxyRequest.emit('pass');
 		}
 
+		if (result.cancel) {
+			proxyRequest.emit('drop');
+		}
+
 		proxyObjects.req[proxyRequest.id] = proxyRequest;
 
-		view.createElement(proxyRequest.id, parsedURL);
+		if (result.display !== false || utils.config('basicRule').beforeRequests || utils.config('basicRule').afterResponses) {
+			view.createElement(proxyRequest.id, parsedURL);
+		}
 
 	}).listen(utils.config('port'));
 };
+
+function executeExtension(request) {
+	var result = { };
+
+	result = utils.merge(result, request);
+
+	result = utils.merge(result, ExtensionManager.exec(ExtensionType.ON_REQUEST, request.headers));
+
+	return result;
+}
